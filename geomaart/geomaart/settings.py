c72443b055e7cc05b353aec04a51853bd3a65c48
java_learing <aswinsandeep4@gmail.com>
2024-12-11 11:57:11 +0530

    first commit on user listing branch were admin can list the users

--- geomaart/geomaart/settings.py
+++ geomaart/geomaart/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'accounts',
     'home',
-    
+    'admin_custom',
     #googleauth files
     'allauth',
     'allauth.account',
@@ -128,22 +128,22 @@ WSGI_APPLICATION = 'geomaart.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'geomart',    
-        'USER': 'aswingeo',   
-        'PASSWORD': 'geo@123',     
-        'HOST': 'localhost',             
-        'PORT': '5432',                 
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'geomart',    
+#         'USER': 'aswingeo',   
+#         'PASSWORD': 'geo@123',     
+#         'HOST': 'localhost',             
+#         'PORT': '5432',                 
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 

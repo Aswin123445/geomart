43d4e2aa9b31cfa58c3e63999f864ccb62653d8e
java_learing <aswinsandeep4@gmail.com>
2024-12-12 17:42:33 +0530

    admin feature to delete user ajaz feature included

--- geomaart/admin_custom/urls.py
+++ geomaart/admin_custom/urls.py
@@ -5,5 +5,6 @@ app_name = 'custom_admin'
 urlpatterns = [
     path('dashboard/',views.dashboard,name='dashboard'),
     path('logout/',views.logout,name='logout'),
-    path('user_list/',views.user_list,name='user_list')
+    path('user_list/',views.user_list,name='user_list'),
+    path('delete_user/<int:id>/',views.delete_user,name='delete_user')
 ]
--- geomaart/admin_custom/views.py
+++ geomaart/admin_custom/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth.decorators import login_required
 from .utils import prevent_cache_view
 from accounts.models import UserData
 from django.core.paginator import Paginator
+from django.http import JsonResponse
+from django.contrib.auth.models import User
 # Create your views here.
 
 def dashboard(request):
@@ -25,7 +27,21 @@ def logout(request):
 def user_list(request):
     current_page_number=request.GET.get('page',1)
     all_user = UserData.objects.all()
-    paginator = Paginator(all_user,3)
+    paginator = Paginator(all_user,2)
     page_obj=paginator.get_page(current_page_number)
     context = {'user_data':page_obj}
     return render(request,'admin_template/user_management/user_list.html',context)
+
+def delete_user(request,id):
+    print('ehlo')
+    if request.method == 'POST' :
+        try :
+            print('inside try block')
+            user = UserData.objects.get(id = id)
+            name= user.name
+            user.delete()
+            print('user deleted')
+            return JsonResponse({'success':True,'message':f'{name} \'s record was successfully deleted'})
+        except User.DoesNotExist :
+            return JsonResponse({"success": False, "message": "User not found."})
+    return JsonResponse({"success": False, "message": "Invalid request method."})

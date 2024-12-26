9947cbcd179050c91026b985a292ad44a3bdb0bd
java_learing <aswinsandeep4@gmail.com>
2024-12-26 20:12:26 +0530

    completed checkout page funtionality in

--- geomaart/cart/urls.py
+++ geomaart/cart/urls.py
@@ -5,5 +5,6 @@ app_name = 'cart'
 urlpatterns = [
     path('product_to_cart/<slug:slug>',views.product_to_cart,name='product_to_cart'),
     path('cart_page/',views.cart_page,name='cart_page'),
-    path('delete_cart_item/<int:id>/',views.delete_cart_item,name='delete_cart_item')
+    path('delete_cart_item/<int:id>/',views.delete_cart_item,name='delete_cart_item'),
+    path('checkout/<int:id>/',views.checkout,name='checkout')
 ]
--- geomaart/cart/views.py
+++ geomaart/cart/views.py
@@ -1,13 +1,13 @@
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.shortcuts import redirect,render
-from accounts.models import UserData
 from .models import Cart,CartItem
 from admin_custom.models import Product
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404
 from .forms import CartItemForm
+from accounts.models import Address
 
 # Create your views here.
 @login_required
@@ -16,19 +16,7 @@ def product_to_cart(request, slug):
     if product.stock == 0 :
         messages.error(request, 'Out of stock. Please stay tuned; the product will be added soon.')
         return redirect('home:product_details')
-
-    #quantity checking
-    checker = Cart.objects.get(user=request.user).items.filter(product = product).first()
-    if checker and not checker.quantity<product.stock :
-        messages.warning(request,'we don\'t  that much product for know')
-        return redirect('home:product_details',slug)
-    if checker and checker.quantity == 10 :
-        messages.warning(request,'maxium quatity limited exeeded')
-        return redirect('home:product_details',slug)
     cart, created = Cart.objects.get_or_create(user=request.user)
-    if CartItem.objects.filter(cart=cart).count() >= 20:
-        messages.error(request, 'Your cart is full.')
-        return redirect('home:product_details',slug)
     cart_item, created = CartItem.objects.update_or_create(
         cart=cart,
         product=product,
@@ -39,6 +27,17 @@ def product_to_cart(request, slug):
             .exists() else 1
         }
     )
+    #quantity checking
+    checker = Cart.objects.filter(user=request.user).first().items.filter(product = product).first()
+    if checker and not checker.quantity<product.stock :
+        messages.warning(request,'we don\'t  that much product for know')
+        return redirect('home:product_details',slug)
+    if checker and checker.quantity == 10 :
+        messages.warning(request,'maxium quatity limited exeeded')
+        return redirect('home:product_details',slug)
+    if CartItem.objects.filter(cart=cart).count() >= 20:
+        messages.error(request, 'Your cart is full.')
+        return redirect('home:product_details',slug)
     cart_item.save()
     CartItem.objects.select_related('items')
     if created:
@@ -63,8 +62,12 @@ def cart_page(request):
         else :
             messages.error(request,list(form.errors.values())[0][0])
     cart=Cart.objects.get(user = request.user) 
+    for car in cart.items.all() :
+        if car.product.is_active == False or car.product.stock < 1 :
+            car.delete()
+            messages.warning(request,'some product are removed from cart because it is not delivarable')
     total_price = sum(list(cart.items.values_list('total_price',flat=True)))
-    context = {'cartitem':cart.items.all(),'sub_toal':total_price}
+    context = {'cartitem':cart.items.all(),'sub_toal':total_price,'user_cart':cart.id}
     return render(request,'cart/cart_page.html',context)
 
 @login_required
@@ -78,3 +81,17 @@ def delete_cart_item(request,id):
         except Exception :
             return JsonResponse({"success": False, "message": "User not found."})
     return JsonResponse({"success": False, "message": "Invalid request method."})
+
+
+#checkout page 
+def checkout(request , id):
+    cart = Cart.objects.get(id = id)
+    if not cart.user.is_phone_number_verified :
+        messages.warning(request,'please verify your phonenumber before checkout')
+        return redirect('home:user_profile')
+    cart_item = CartItem.objects.filter(cart = cart)
+    total_sum = sum(cart_item.values_list('total_price',flat=True))
+    print(total_sum)
+    address =  Address.objects.filter(user = request.user)
+    context = {'address':address ,'total_sum':total_sum,'cart_item':cart_item}
+    return render(request,'checkout/checkout.html',context)
